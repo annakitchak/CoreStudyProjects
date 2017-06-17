101d0b4e26e8fcbcd61338dc1310dfb8c673bf7d
Anna Tarnavchyk <annakitchak@gmail.com>
2017-06-17 14:51:41 +0300

    SecondLesson

---
+++ SecondLesson/src/ua/less2/Main.java
@@ -0,0 +1,105 @@
+package ua.less2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		//if-else
+		
+		//Task1
+//		int n = 22;
+//		if(n%2==0){
+//			System.out.println("Число парне!");
+//		}else{
+//			System.out.println("Число не парне!");
+//		}
+		
+		//Task2
+//		double m = 8.5;
+//		double n = 11.25;
+//		double res1, res2;
+//		res1 = 10-m;
+//		res2 = 10-n;
+//		if (res1<0){
+//			res1=res1*(-1);
+//		}
+//		if (res2<0){
+//			res2=res2*(-1);
+//		}
+//		if(res1<res2){
+//			System.out.println("Ближче до десяти число "+m);
+//		}else{
+//			System.out.println("Ближче до десяти число "+n);
+//		}
+		
+		
+		//Task3
+//		double a=2;
+//		double b=8;
+//		double c=4;
+//		double d;
+//		double x1;
+//		double x2;
+//		d=Math.sqrt(Math.pow(b, 2)-4*a*c);
+//		if(d!=0){
+//			x1=(-b+d)/2*a;
+//			x2=(-b-d)/2*a;
+//			System.out.println("x1= " +x1);
+//			System.out.println("x2= " +x2);
+//		}else{
+//			System.out.println("Коренів немає!");
+//		}
+		
+		
+		
+		//LOOPS
+		//Task1
+//		int a=1000;
+//		int b=9997;
+//		do{
+//			a=a+3;
+//			System.out.println(a);
+//		}while (a<b);
+		
+		//Task2
+//		int a=1;
+//		int count=0;
+//		System.out.println(a);
+//		for(int i=0;i<54;i++){
+//			count++;
+//		do{
+//			a=a+2;
+//			System.out.println(a);
+//		}while(a<count);
+//		}
+		
+		//Task3
+//		int a=90;
+//		System.out.println(a);
+//		do{
+//			a=a-5;
+//			System.out.println(a);
+//		}while(a>0);
+		
+		//Task4
+//		int a=2;
+//		int count=0;
+//		System.out.println(a);
+//		for(int i=0;i<19;i++){
+//			count++;
+//		do{
+//			a=a*2;
+//			System.out.println(a);
+//		}while(a<count);
+//		}
+		
+		//Task5
+		
+		
+		
+		
+		
+		
+	}
+
+}
