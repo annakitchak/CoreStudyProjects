dbb4d273c00f01d34766c6dc94ccd79888c00b92
Anna Tarnavchyk <annakitchak@gmail.com>
2017-06-17 13:41:15 +0300

    Generic

---
+++ Generic/src/ua/study/Main.java
@@ -0,0 +1,28 @@
+package ua.study;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person<String,Integer> person = new Person<String, Integer>("olga", 12);
+		Person<Long, Byte> person2 = new Person<Long, Byte>(1000L, (byte)(20));
+		System.out.println(person.getName().getClass().getSimpleName().equals("String")); //якого типу даних поле у конкретного person
+		System.out.println(person.getName() instanceof String);
+		List<Person> list = new ArrayList<Person>();
+		list.add(person);
+		list.add(person2);
+		double sum =0;
+		for (Person p : list) {
+			if(p.getName() instanceof Number){
+				sum+=Double.parseDouble(String.valueOf(p.getName())) ;
+			}
+			sum+= Double.parseDouble(String.valueOf(p.getAge())) ;
+		}
+		System.out.println(sum);
+		
+	}
+
+}
